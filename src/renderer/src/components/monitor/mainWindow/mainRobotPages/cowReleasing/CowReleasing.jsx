befe7c13cd68f8f76a95d2a15421719a34e636a3
MaksimRumiantsevPromtechnika <maksim.rumiantsev@promtechnika.com>
2024-06-06 17:58:04 +0300

    cow releasing fix

--- src/renderer/src/components/monitor/mainWindow/mainRobotPages/cowReleasing/CowReleasing.jsx
+++ src/renderer/src/components/monitor/mainWindow/mainRobotPages/cowReleasing/CowReleasing.jsx
@@ -14,34 +14,39 @@ const CowReleasing = () => {
   return (
     <>
       <div className="status-cow-in-cell-bar">
-        <div className="status-wait-icon home-status-icon" style={{ display: (allState.i161 === 0 && main == "1") ? "" : "none" }} />
-        <div className="status-cow-in-cell-icon home-status-icon" style={{ display: (allState.i161 === 0 && main == "7") ? "" : "none" }} />
-        <div className="status-cow-icon home-status-icon" style={{ display: ((main == "8" && (allState.i598 === 0 && allState.i599 === 0) && (((milkingInfo.length - 1) >= 0 && (milkingInfo[milkingInfo.length - 1].milk).toFixed(1) == 0) || (milkingInfo.length) == 0) && allState.i161 === 0 && main == "8") || ((allState.i161 === 0 && main == "8" && ((allState.i598 === 1) || allState.i599 === 1)) && (milkingInfo.length) == 0)) ? "" : "none" }} />
-        <div className="status-cow-milking-icon home-status-icon" style={{ display: ((main == "8" && (milkingInfo.length - 1) >= 0 && (milkingInfo[milkingInfo.length - 1].milk).toFixed(1) > 0) && allState.i161 === 0) && allState.i161 === 0 ? "" : "none" }} />
-        <div className="pump-icon home-status-icon" style={{ display: allState.i161 === 1 ? "" : "none" }} />
-        <div className="cow-in-cell-id" style={{ display: (allState.i161 === 1 ? "" : "none") }}>{pastCow}</div>
-        <div className="cow-in-cell-id" style={{ display: ((allState.i161 === 1 || main == "1") ? "none" : "") }}>{cowInfo.id}</div>
+        <div className="status-wait-icon home-status-icon" style={{ display: (allState.i41 === 0 && main == "1") ? "" : "none" }} />
+        <div className="status-cow-in-cell-icon home-status-icon" style={{ display: (allState.i41 === 0 && main == "7") ? "" : "none" }} />
+        <div className="status-cow-icon home-status-icon" style={{ display: ((main == "8" && (allState.i39 === 0 && allState.i38 === 0) && (((milkingInfo.length - 1) >= 0 && (milkingInfo[milkingInfo.length - 1].milk).toFixed(1) == 0) || (milkingInfo.length) == 0) && allState.i41 === 0 && main == "8") || ((allState.i41 === 0 && main == "8" && ((allState.i39 === 1) || allState.i38 === 1)) && (milkingInfo.length) == 0)) ? "" : "none" }} />
+        <div className="status-cow-milking-icon home-status-icon" style={{ display: ((main == "8" && (milkingInfo.length - 1) >= 0 && (milkingInfo[milkingInfo.length - 1].milk).toFixed(1) > 0) && allState.i41 === 0) && allState.i41 === 0 ? "" : "none" }} />
+        <div className="pump-icon home-status-icon" style={{ display: allState.i41 === 1 ? "" : "none" }} />
+        <div className="cow-in-cell-id" style={{ display: (allState.i41 === 1 ? "" : "none") }}>{pastCow}</div>
+        <div className="cow-in-cell-id" style={{ display: ((allState.i41 === 1 || main == "1") ? "none" : "") }}>{cowInfo.id}</div>
       </div>
 
-      <div className="status-milk-transport-bar" style={{ display: ((main == "1" && (allState.i598 === 1 || allState.i599 === 1)) || (main == "7") || (main == "8" && (allState.i598 === 1 || allState.i599 === 1) && (milkingInfo.length) == 0)) || allState.i161 === 1 ? "" : "none" }}>
-        <div className="status-milk-transport-status">
-          <div className="status-milk-status transport-status-pump" style={{ display: (allState.i598 === 1 || allState.i599 === 1) || allState.i161 === 1 ? "" : "none" }} />
-          <div className="status-milk-status transport-status-ok" style={{ display: (allState.i598 === 0 && allState.i599 === 0) && allState.i161 === 0 ? "" : "none" }} />
+      <div className="status-milk-transport-bar" style={{ display: ((main == "1" && (allState.i39 === 1 || allState.i38 === 1)) || (main == "7") || (main == "8" && (allState.i39 === 1 || allState.i38 === 1) && (milkingInfo.length) == 0)) || allState.i41 === 1 ? "" : "none" }}>
+        <div className="status-milk-bar" style={{ display: "flex" }}>
+          <div className="status-milk-transport-status">
+            <div className="status-milk-status transport-status-pump" style={{ display: (allState.i39 === 1 || allState.i38 === 1) || allState.i41 === 1 ? "" : "none" }} />
+            <div className="status-milk-status transport-status-ok" style={{ display: (allState.i39 === 0 && allState.i38 === 0) && allState.i41 === 0 ? "" : "none" }} />
+          </div>
+          <div className="status-milk-transport-arrow" />
+          <div className="status-milk-transport-place">
+            <div className="status-milk-transport transport-tanker" style={{ display: (cowInfo.milkDestination === 0 ? "" : "none") }} />
+            <div className="status-milk-transport transport-drain" style={{ display: (cowInfo.milkDestination === 2 ? "" : "none") }} />
+            <div className="status-milk-transport transport-backet" style={{ display: (cowInfo.milkDestination === 1 ? "" : "none") }} />
+          </div>
         </div>
-        <div className="status-milk-transport-arrow" />
-        <div className="status-milk-transport-place">
-          <div className="status-milk-transport transport-tanker" style={{ display: (cowInfo.milkDestination === 0 ? "" : "none") }} />
-          <div className="status-milk-transport transport-drain" style={{ display: (cowInfo.milkDestination === 2 ? "" : "none") }} />
-          <div className="status-milk-transport transport-backet" style={{ display: (cowInfo.milkDestination === 1 ? "" : "none") }} />
+        <div className="conductivity-bar" style={{ display: "flex", fontSize: "35px" }}>
+          <div className="conductivity"></div>
+          <div className="conductivity-value" style={{ marginLeft: "10px" }}>22%</div>
         </div>
       </div>
-      <div className="milking-bar" style={{ display: (((main == "8" && (allState.i598 === 0 && allState.i599 === 0) && (milkingInfo.length) == 0) || (main == "8" && (milkingInfo.length) > 0)) && allState.i161 === 0) ? "" : "none" }}>
+      <div className="milking-bar" style={{ display: (((main == "8" && (allState.i39 === 0 && allState.i38 === 0) && (milkingInfo.length) == 0) || (main == "8" && (milkingInfo.length) > 0)) && allState.i41 === 0) ? "" : "none" }}>
         <div className="milking-path transport-tanker" style={{ display: (cowInfo.milkDestination === 0 && (milkingInfo.length - 1) >= 0 && (milkingInfo[milkingInfo.length - 1].milk).toFixed(1) > 0 ? "" : "none") }} />
         <div className="milking-path transport-backet" style={{ display: ((cowInfo.milkDestination === 1 || ((milkingInfo.length - 1) >= 0 && (milkingInfo[milkingInfo.length - 1].milk).toFixed(1) == 0) || (milkingInfo.length) == 0) ? "" : "none"), marginRight: "12px" }} />
         <div className="milking-path transport-drain" style={{ display: ((cowInfo.milkDestination === 2 && (milkingInfo.length - 1) >= 0 && (milkingInfo[milkingInfo.length - 1].milk).toFixed(1) > 0) ? "" : "none") }} />
         <div className="milking-amount">{milkingInfo.length == 0 ? "0,0" : (((milkingInfo[milkingInfo.length - 1].milk).toFixed(1) / 30).toFixed(1)).replace(".", ",")}</div>
       </div>
-
       <button className="button graph-swap sidetouch" style={{ display: (main == "8") ? "" : "none" }} onClick={() => dispatch(toggleMilking())} />
     </>
   );
