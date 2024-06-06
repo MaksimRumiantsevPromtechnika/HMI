719e3a5870de077e5dd4b12301e4ad665808bc9a
MaksimRumiantsevPromtechnika <maksim.rumiantsev@promtechnika.com>
2024-06-06 17:59:06 +0300

    update backet page

--- src/renderer/src/components/monitor/mainWindow/milkPathControl/MilkPathControl.jsx
+++ src/renderer/src/components/monitor/mainWindow/milkPathControl/MilkPathControl.jsx
@@ -1,3 +1,4 @@
+import { red } from '@mui/material/colors';
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearBucket } from '../../../../store/bucketReducer';
@@ -14,19 +15,32 @@ const MilkPathControl = () => {
 
   const bucketsInfo = useSelector(state => state.buckets.buckets)
 
+  const warningColor = {
+    "Мастит": "red",
+    "Молозево": "orange",
+    "Антибиотик": "green"
+  }
 
   for (let i = 0; i < backetValue; i++) {
-    pathList.push(<div style={{ display: "flex", flexDirection: "column", gap: "52px", alignItems: "center", }}>
-      <button onClick={(e) => swapBucket(e)} style={{ backgroundColor: (i + 1) == selectedBucket ? "#25be2c" : "white" }} className="button path-list-button" id={i + 1} key={i + 1}>{i + 1}</button><div style={{ fontSize: "25px", height: "31px", display: "flex", justifyContent: 'center' }}>{bucketsInfo[i].status}</div>
-      <div className='milk-path-setting-item'>
-        <div className='milk-path-value'>{bucketsInfo[i].id}</div>
+    pathList.push(<div style={{ display: "flex", flexDirection: "column", alignItems: "center", }}>
+      <button onClick={(e) => swapBucket(e)} style={{ backgroundColor: (i + 1) == selectedBucket ? 'rgba(42, 86, 154, 0.66)' : "white" }} className="button path-list-button" id={i + 1} key={i + 1}>{i + 1}</button>
+      <div style={{ fontSize: "25px", height: "31px", display: "flex", justifyContent: 'center', marginTop: "52px", backgroundColor: warningColor[bucketsInfo[i].status] }}>{bucketsInfo[i].status}</div>
+      <div className='milk-path-setting-item-id' style={{ marginTop: "33px" }}>
+        <div className='milk-path-setiing-id'>
+          <div className='milk-path-value'>{bucketsInfo[i].id}</div>
+          <div className='milk-path-value'>{bucketsInfo[i].id}</div>
+        </div>
+        <div className='milk-path-setiing-id'>
+          <div className='milk-path-value'>{bucketsInfo[i].id}</div>
+          <div className='milk-path-value'>{bucketsInfo[i].id}</div>
+        </div>
       </div>
-      <div className='milk-path-setting-item'>
+      <div className='milk-path-setting-item' style={{ marginTop: "39px" }}>
         <div className='milk-path-value'>{bucketsInfo[i].val}</div>
       </div>
       <div className='milk-path-setting-item'>
 
-        <div className='milk-path-value'>{bucketsInfo[i].dateTime}</div>
+        <div className='milk-path-value' style={{ marginTop: "52px" }}>{bucketsInfo[i].dateTime}</div>
       </div>
     </div>
     )
@@ -41,7 +55,7 @@ const MilkPathControl = () => {
         <div className='milk-path-icon milk-path-timer-icon'></div>
         <button className="button milk-path-drain-button" onClick={() => dispatch(clearBucket(selectedBucket))}></button>
       </div>
-      <div style={{ height: "100%", display: "flex", justifyContent: "center", gap: "50px", marginLeft: '30px' }} className="BacketsList">
+      <div style={{ height: "100%", display: "flex", justifyContent: "center", gap: "25px", marginLeft: '30px' }} className="BacketsList">
         {pathList}
       </div>
     </div>
