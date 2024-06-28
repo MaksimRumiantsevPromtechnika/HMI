379b2e9b46b75fa269865e4f04c7936a6219e8ad
MaksimRumiantsevPromtechnika <maksim.rumiantsev@promtechnika.com>
2024-06-28 15:19:35 +0300

    teat calibration

--- src/renderer/src/store/cupsInfoReducer.js
+++ src/renderer/src/store/cupsInfoReducer.js
@@ -1,17 +1,18 @@
 const defaultState = {
   teatsInfo: {
-    s1: "1",
-    x1: "-44",
-    y1: "108",
-    s2: "2",
-    x2: "-59",
-    y2: "115",
-    s3: "0",
-    x3: "57",
-    y3: "232",
-    s4: "0",
-    x4: "92",
-    y4: "136",
+    teat1:  {x: -44, y: 108, status: 2},
+    teat2:  {x: "-59", y: "115", status: 2},
+    teat3:  {x: "57", y: "232", status: 7},
+    teat4:  {x: "92", y: "136", status: 2},
+    teat0: [
+      {x: -66, y: 128, status: 0},
+      {x: -48, y: 240, status: 0},
+      {x: 55, y: 255, status: 0},
+      // { x: -66, y: -128 },
+      // { x: -48, y: -240 },
+      // { x: 55, y: -255 },
+      // { x: 88, y: -132 },
+    ]
   }
 }
 const UPDATE_TEAT_INFO = "UPDATE_TEAT_INFO"
