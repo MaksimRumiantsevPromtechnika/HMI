56f566e5d1fbb8aa6aca96fb520196959983eff1
MaksimRumiantsevPromtechnika <maksim.rumiantsev@promtechnika.com>
2024-06-28 15:18:52 +0300

    cow lock timer

--- src/renderer/src/components/monitor/mainWindow/mainRobotPages/cowLock/cowLock.jsx
+++ src/renderer/src/components/monitor/mainWindow/mainRobotPages/cowLock/cowLock.jsx
@@ -1,13 +1,22 @@
 const CowLock = () => {
   return (
     <>
-      <div className="status-cow-in-cell-bar">
-        <div className="status-cow-close-icon home-status-icon" />
-        <div className="cow-in-cell-id">1234</div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div className='timer'>
+          <div className='timer-icon'>
+          </div>
+          <p className='timer-count'>{"0:00"}</p>
+        </div>
       </div>
-      <div className="status-milk-transport-bar">
-        <button className="milking-touch status-cow-milking-icon sidetouch"></button>
-        <p className="double-milking" style={{ fontSize: "38px", width: "60px", textAlign: 'center' }}>2X</p>
+      <div style={{ display: "flex", marginTop: "50px" }}>
+        <div className="status-cow-in-cell-bar">
+          <div className="status-cow-close-icon home-status-icon" />
+          <div className="cow-in-cell-id">1234</div>
+        </div>
+        <div className="status-milk-transport-bar" style={{ display: "flex", flexDirection: "column", alignItems: "cen" }}>
+          <button className="milking-touch status-cow-milking-icon sidetouch"></button>
+          <div className="double-milking" style={{ fontSize: "38px", width: "60px", marginLeft: "45px" }}>2X</div>
+        </div>
       </div>
     </>
   );
